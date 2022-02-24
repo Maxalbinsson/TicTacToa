fc82edc0261d3bcf06bbb60dfc5b6117214e6677
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-02-24 09:14:06 +0100

    :)

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+     char[][] board;
+
+    {
+        board = new char[3][3];
+    }
+
+    public static void main(String[] args) {
+    }
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            board[i][j] = '-';
+        }
+        private class scanner {
+        }
+
+        Object system;
+        scanner in = new/system.in);
+
+
+
+
 }
