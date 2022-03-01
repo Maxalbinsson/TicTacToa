d8e5de848d93ac73159790efa97bf738a4834887
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2022-03-01 15:09:12 +0100

    :)

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
-public class TicTacToe {
-     char[][] board;
+public class TicTacToe<i> {
+    private static Scanner in;
+    char[][] board;
 
     {
         board = new char[3][3];
     }
 
     public static void main(String[] args) {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+   
+        }
+        class scanner {
+            final Scanner in = new Scanner(System.in);
+		System.out.print("Spelare 1, vad är ditt namn? ");
+            final String p1 = in.nextLine();
+		System.out.print("Spelare 2, vad är ditt namn? ");
+        String p2 = in.nextLine();
     }
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            board[i][j] = '-';
-        }
-        private class scanner {
-        }
-
-        Object system;
-        scanner in = new/system.in);
 
 
 
+}
 
 }
